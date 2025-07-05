fb8fdbaec6a135bd7fae042d724639ca1392167d
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2025-07-05 15:40:52 +0530

    adding for loop example

--- forloop.go
+++ forloop.go
@@ -14,10 +14,46 @@ func main() {
 	 Iinitial statement and the post statement from the for syntax, and only use the condition
 	*/
 
+	/*
 	i := 0
 
 	for i < 5 {
 		fmt.Println(i)
 		i++
 	}
+	*/
+
+	
+	/*
+	buf := bytes.NewBufferString("one\ntwo\nthree\nfour\n")
+
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+
+				fmt.Print(line)
+				break
+			}
+			fmt.Println(err)
+			break
+		}
+		fmt.Print(line)
+	}
+	*/
+
+	sharks := []string{"hammerhead", "great white", "dogfish", "frilled", "bullhead", "requiem"}
+/*
+	for i := 0; i < len(sharks); i++ {
+		fmt.Println(sharks[i])
+	}
+*/
+
+/*
+	Using Range clause
+*/
+for i, shark := range sharks {
+		fmt.Println(i, shark)
+	}
+
 }
