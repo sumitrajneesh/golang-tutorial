689ba3672c6195559c0a4ca6386c84074913e705
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2025-07-13 17:11:29 +0530

    if/else

--- forloop.go
+++ forloop.go
@@ -1,59 +1,59 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	/*
-	simple loop
-	*/
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println("Hi go")
-	// }
+// func main() {
+// 	/*
+// 	simple loop
+// 	*/
+// 	// for i := 0; i < 3; i++ {
+// 	// 	fmt.Println("Hi go")
+// 	// }
 
-	/*
-	 Iinitial statement and the post statement from the for syntax, and only use the condition
-	*/
+// 	/*
+// 	 Iinitial statement and the post statement from the for syntax, and only use the condition
+// 	*/
 
-	/*
-	i := 0
+// 	/*
+// 	i := 0
 
-	for i < 5 {
-		fmt.Println(i)
-		i++
-	}
-	*/
+// 	for i < 5 {
+// 		fmt.Println(i)
+// 		i++
+// 	}
+// 	*/
 
 	
-	/*
-	buf := bytes.NewBufferString("one\ntwo\nthree\nfour\n")
+// 	/*
+// 	buf := bytes.NewBufferString("one\ntwo\nthree\nfour\n")
 
-	for {
-		line, err := buf.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
+// 	for {
+// 		line, err := buf.ReadString('\n')
+// 		if err != nil {
+// 			if err == io.EOF {
 
-				fmt.Print(line)
-				break
-			}
-			fmt.Println(err)
-			break
-		}
-		fmt.Print(line)
-	}
-	*/
+// 				fmt.Print(line)
+// 				break
+// 			}
+// 			fmt.Println(err)
+// 			break
+// 		}
+// 		fmt.Print(line)
+// 	}
+// 	*/
 
-	sharks := []string{"hammerhead", "great white", "dogfish", "frilled", "bullhead", "requiem"}
-/*
-	for i := 0; i < len(sharks); i++ {
-		fmt.Println(sharks[i])
-	}
-*/
+// 	sharks := []string{"hammerhead", "great white", "dogfish", "frilled", "bullhead", "requiem"}
+// /*
+// 	for i := 0; i < len(sharks); i++ {
+// 		fmt.Println(sharks[i])
+// 	}
+// */
 
-/*
-	Using Range clause
-*/
-for i, shark := range sharks {
-		fmt.Println(i, shark)
-	}
+// /*
+// 	Using Range clause
+// */
+// for i, shark := range sharks {
+// 		fmt.Println(i, shark)
+// 	}
 
-}
+// }
---
+++ if/else.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+ age := 20
+ if age >= 18 {
+ fmt.Println("You are an adult")
+ } else {
+ fmt.Println("You are still young")
+ }
+}
